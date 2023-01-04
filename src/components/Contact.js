4ddf00a1e301f2dcd836b0ddc855665a633c6382
Yogesh <chikutavethiya97@gmail.com>
2023-01-04 22:07:29 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <main>
+        <h1>Contact Us</h1>
+        
+        <form>
+
+            <div>
+            <label>Name</label>
+            <input type="text" required placeholder='ABC' />
+            </div>
+
+            <div>
+            <label>Email</label>
+            <input type="email" required placeholder='abc@xyz.com' />
+            </div>
+
+            <div>
+            <label>Message</label>
+            <input type="text" required placeholder='Your query here...' />
+            </div>
+
+            <button type='submit'>Send</button>
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default Contact
